82bb79561ec4555450f119a51952e376b65e4ec8
Wlodek Bzyl <matwb@univ.gda.pl>
2018-10-15 19:49:04 +0200

    Update factorial.rb

--- ruby/factorial.rb
+++ ruby/factorial.rb
@@ -1,21 +1,22 @@
 def fact(n)
-  n.zero? ? 1 : n * fact(n - 1)
-  # if n.zero?
-  #   return 1
-  # else
-  #   return n * fact(n - 1)
-  # end
+  # n.zero? ? 1 : n * fact(n - 1)
+  if n.zero?
+    return 1
+  else
+    return n * fact(n - 1)
+  end
 end
 
 # add new method to builtin class
+
 class Integer
   def fact
-    zero? ? 1 : self * (self - 1).fact
-    # if zero?
-    #   return 1
-    # else
-    #   return self * (self - 1).fact
-    # end
+    #  zero? ? 1 : self * (self - 1).fact
+    if zero?
+      return 1
+    else
+      return self * (self - 1).fact
+    end
   end
 end
 
